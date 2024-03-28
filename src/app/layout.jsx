a1e49af736d1872d524236f4947763e8b504d308
Samuel Ortiz <samuelortiz@MacBook-Pro-de-Samuel.local>
2024-03-28 11:53:05 -0500

    error handled

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import { Footer } from "./components/Footer";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -12,8 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${manrope.className} flex flex-col justify-between min-h-screen bg-[rgb(242,242,247)] dark:bg-[rgb(28,28,30)] dark:text-white text-black`}>
-        {children}
-        <Footer/>
+        <ErrorBoundary>
+          {children}
+          <Footer/>
+        </ErrorBoundary>
       </body>
     </html>
   );
