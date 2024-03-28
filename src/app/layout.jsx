a07e1725011ff1140b25fe0e43e5216ffea86d03
Samuel Ortiz <samuelortiz@MacBook-Pro-de-Samuel.local>
2024-03-28 12:09:09 -0500

    localStorage disabled

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,6 @@
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import { Footer } from "./components/Footer";
-import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -9,14 +8,13 @@ export const metadata = {
   title: "The To Do Web",
 };
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${manrope.className} flex flex-col justify-between min-h-screen bg-[rgb(242,242,247)] dark:bg-[rgb(28,28,30)] dark:text-white text-black`}>
-        <ErrorBoundary>
           {children}
           <Footer/>
-        </ErrorBoundary>
       </body>
     </html>
   );
